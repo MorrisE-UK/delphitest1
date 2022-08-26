4e0e4fd0d2445b292c8ec9b1af327727aa73aca0
MorrisE-UK <112174869+MorrisE-UK@users.noreply.github.com>
2022-08-26 12:59:24 +0100

    Add files via upload
    The program has been left as default "Project1"/"Unit1", and the exe is in Win32/Debug.
    Many other solutions are possible, as per notes in Unit1.

---
+++ Unit1.dfm
@@ -0,0 +1,134 @@
+object frmImportFile: TfrmImportFile
+  Left = 0
+  Top = 0
+  Caption = 'Import File'
+  ClientHeight = 412
+  ClientWidth = 621
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Menu = MainMenu1
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnClose = FormClose
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object StatusBar1: TStatusBar
+    Left = 0
+    Top = 393
+    Width = 621
+    Height = 19
+    Panels = <>
+    ExplicitWidth = 633
+  end
+  object GroupBox1: TGroupBox
+    Left = 0
+    Top = 0
+    Width = 621
+    Height = 113
+    Align = alTop
+    Caption = 'Locate file'
+    TabOrder = 1
+    ExplicitWidth = 633
+    object lblImportFile: TLabel
+      Left = 32
+      Top = 35
+      Width = 51
+      Height = 13
+      Caption = 'Import File'
+      FocusControl = edtImportFile
+    end
+    object edtImportFile: TEdit
+      Left = 89
+      Top = 32
+      Width = 320
+      Height = 21
+      TabOrder = 0
+      Text = 'C:\Temp\ExampleOperatorServiceDaysOfOp.csv'
+    end
+    object btnBrowse: TBitBtn
+      Left = 423
+      Top = 30
+      Width = 75
+      Height = 25
+      Caption = '&Browse'
+      TabOrder = 1
+      OnClick = btnBrowseClick
+    end
+    object btnImport: TBitBtn
+      Left = 89
+      Top = 59
+      Width = 75
+      Height = 25
+      Caption = '&Import'
+      TabOrder = 2
+      OnClick = btnImportClick
+    end
+  end
+  object TreeView1: TTreeView
+    Left = 0
+    Top = 113
+    Width = 423
+    Height = 280
+    Align = alClient
+    Indent = 19
+    TabOrder = 2
+    ExplicitLeft = 32
+    ExplicitTop = 161
+    ExplicitWidth = 273
+  end
+  object ListBox1: TListBox
+    Left = 423
+    Top = 113
+    Width = 198
+    Height = 280
+    Align = alRight
+    ItemHeight = 13
+    TabOrder = 3
+    Visible = False
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 272
+    Top = 136
+  end
+  object MainMenu1: TMainMenu
+    Left = 392
+    Top = 144
+    object File1: TMenuItem
+      Caption = '&File'
+      object mnuImport: TMenuItem
+        Caption = '&Import'
+        OnClick = btnImportClick
+      end
+      object N2: TMenuItem
+        Caption = '-'
+      end
+      object Print1: TMenuItem
+        Caption = '&Print...'
+        Visible = False
+      end
+      object PrintSetup1: TMenuItem
+        Caption = 'P&rint Setup...'
+        Visible = False
+      end
+      object N1: TMenuItem
+        Caption = '-'
+      end
+      object Exit1: TMenuItem
+        Caption = 'E&xit'
+        OnClick = Exit1Click
+      end
+    end
+    object Help1: TMenuItem
+      Caption = '&Help'
+      object About1: TMenuItem
+        Caption = '&About...'
+        OnClick = About1Click
+      end
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,255 @@
+unit Unit1;
+{
+There are so many possible ways to implement this import.
+Even just reading the file, I could have assumed the worst and read it as a binary file.
+Or read it as a normal file, and handled the deliminations and error trapping.
+I could have built in reading from the web, or svn, I suppose.
+I've gone for a neat, if limited use, TStringList read from file.
+
+Each record could obviously, neatly, have been created as an Route object.
+The Companies themselves could have been created as Company objects.
+This would have then lead on to extending the structure, for stops, times, costs, drivers.
+
+Each node in the treeview could then have had the object added to it.
+
+And in the final close, we would have free'ed the objects.
+
+But for the simple purposes of the treeView it was not absolutely necessary.
+Much as we could have added images to the tree.
+Or a ProgressBar, or an option to cancel mid process.
+Or far more enhaned error handling / messaging.
+
+I may also have cheated, by holding the top three nodes as private variables,
+rather than looking for them each time.
+And the ListBox was simply for debugging, easy to remove.
+
+Note, I have also not included any unit testing.
+
+Morris Evans
+26/08/22
+}
+
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, System.StrUtils,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.Menus,
+  Vcl.Buttons;
+
+type
+  TfrmImportFile = class(TForm)
+    StatusBar1: TStatusBar;
+    GroupBox1: TGroupBox;
+    TreeView1: TTreeView;
+    OpenDialog1: TOpenDialog;
+    MainMenu1: TMainMenu;
+    File1: TMenuItem;
+    Print1: TMenuItem;
+    PrintSetup1: TMenuItem;
+    Exit1: TMenuItem;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    Help1: TMenuItem;
+    About1: TMenuItem;
+    mnuImport: TMenuItem;
+    lblImportFile: TLabel;
+    edtImportFile: TEdit;
+    btnBrowse: TBitBtn;
+    btnImport: TBitBtn;
+    ListBox1: TListBox;
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure btnBrowseClick(Sender: TObject);
+    procedure btnImportClick(Sender: TObject);
+    procedure Exit1Click(Sender: TObject);
+    procedure About1Click(Sender: TObject);
+  private
+    { Private declarations }
+    WeekNode, SatNode, SunNode : TTreeNode;
+    procedure AddTopNodes;
+    procedure AddToNode(iTop : integer; strCompany, strRoute : string);
+    function FindCompany(topNode : TTreeNode; strCompany : string) : TTreeNode;
+    function NodePosition(aNode : TTreeNode) : integer;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmImportFile: TfrmImportFile;
+
+implementation
+
+{$R *.dfm}
+
+//Locate the import file:
+procedure TfrmImportFile.btnBrowseClick(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+    edtImportFile.Text := OpenDialog1.FileName;
+
+end;
+
+//Main process of file:
+procedure TfrmImportFile.btnImportClick(Sender: TObject);
+var
+  fileData : TStringList;
+  sArray : TArray<string>;
+  strLine, strRoute, strCompany, strDays : String;
+  lines, i : Integer;
+begin
+  if treeView1.Items.Count > 3 then
+  begin
+    if MessageDlg('Clear existing ?',mtConfirmation, [mbYes,mbNo],0) = mrYes then
+    begin
+       TreeView1.Items.Clear;
+       AddTopNodes;
+    end;
+  end;
+
+  //prevent double hit
+  btnImport.Enabled := false;
+  treeView1.Items.BeginUpdate;
+
+  fileData := TStringList.Create;       // Create the TSTringList object
+  fileData.QuoteChar := '"';
+  try
+    try
+      fileData.LoadFromFile(edtImportFile.Text);    // Load from Testing.txt file
+
+      lines := fileData.Count;
+
+      for i := 0 to lines-1
+      do
+      begin
+        strLine := fileData[i];
+        sArray := strLine.Split([','],'"');
+        //(',');
+        strCompany := sArray[0].DeQuotedString('"');
+        strRoute := sArray[1].DeQuotedString('"');
+        strDays := sArray[2].DeQuotedString('"');
+
+        ListBox1.AddItem(strCompany,nil);
+        ListBox1.AddItem(strRoute,nil);
+        ListBox1.AddItem(strDays,nil);
+
+        if (Pos('1',strDays.Substring(0,5)) > 0 ) then //Weekday
+        begin
+            AddToNode(0, strCompany, strRoute);
+            ListBox1.AddItem('Weekday', nil);
+        end;
+        if (strDays.Substring(5,1) = '1') then
+        begin
+            AddToNode(1, strCompany, strRoute);
+            ListBox1.AddItem('Sat', nil);
+        end;
+        if (strDays.Substring(6,1) = '1') then   // Sunday
+        begin
+            AddToNode(2, strCompany, strRoute);
+            ListBox1.AddItem('Sun', nil);
+        end;
+      end;
+    except on E: Exception do
+      ShowMessage(e.Message);
+    end;
+  finally
+    fileData.free;
+    treeView1.Items.EndUpdate;
+    btnImport.Enabled := true;
+  end;
+end;
+
+//Add a record to the tree:
+procedure TfrmImportFile.About1Click(Sender: TObject);
+begin
+     ShowMessage('Created by Morris Evans.');
+end;
+
+procedure TfrmImportFile.AddToNode(iTop : integer; strCompany, strRoute : string);
+var nodeTop, nodeCompany : TTreeNode;
+//  ThisRoute : TRoute;
+begin
+  case iTop of
+   0 : nodeTop := WeekNode;
+   1 : nodeTop := SatNode;
+   2 : nodeTop := SunNode;
+  else
+    raise Exception.Create('Unknown group');
+  end;
+  // locate the company under the top node.
+  nodeCompany := FindCompany(nodeTop, strCompany);
+  if nodeCompany = nil then
+    nodeCompany := TreeView1.Items.AddChild(nodeTop, strCompany);
+  //then add the route
+  TreeView1.Items.AddChild(nodeCompany, strRoute);
+
+  {
+  ThisRoute := new TRoute
+  TreeView1.Items.AddChildObject(nodeCompany, strRoute, route);
+  }
+end;
+
+//Locate existing company record for the day selection
+function TfrmImportFile.FindCompany(topNode : TTreeNode; strCompany : string) : TTreeNode;
+var i : integer;
+    resultNode : TTreeNode;
+begin
+  resultNode := nil;
+  for i := NodePosition(topNode)+1 to treeView1.items.Count-1 do
+  begin
+    if (treeview1.items[i].Parent <> topNode) AND
+       ( (treeview1.items[i].Parent = WeekNode) OR
+         (treeview1.items[i].Parent = SatNode) OR
+         (treeview1.items[i].Parent = SunNode) )
+       then break;
+    if treeview1.items[i].Text = strCompany then
+    begin
+      resultNode := treeview1.items[i];
+      break;
+    end;
+  end;
+  Result := resultNode;
+end;
+
+//Locate the position of a node in the tree
+function TfrmImportFile.NodePosition(aNode : TTreeNode) : integer;
+var i : integer;
+begin
+  result := 0;
+  for i := 0 to treeView1.items.Count-1 do
+  begin
+    if treeview1.items[i] = aNode then
+    begin
+      result := i;
+      break;
+    end;
+  end;
+end;
+
+
+procedure TfrmImportFile.Exit1Click(Sender: TObject);
+begin
+    Close;
+end;
+
+//Tidy up any objects created:
+procedure TfrmImportFile.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+//
+end;
+
+//Initialise
+procedure TfrmImportFile.FormCreate(Sender: TObject);
+begin
+//
+  AddTopNodes;
+end;
+
+procedure TfrmImportFile.AddTopNodes;
+begin
+  WeekNode := TreeView1.Items.add(nil,'Monday - Friday');
+  SatNode := TreeView1.Items.add(nil,'Saturday');
+  SunNode := TreeView1.Items.add(nil,'Sunday');
+end;
+
+end.
